c7e363158e7e3d4db4151c5e48630610cfb2bec9
natoliveira <nataliadeolive@gmail.com>
2021-06-30 11:25:06 -0300

    implementacao n rainhas

---
+++ cmake-build-debug/CMakeFiles/untitled34.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/untitled34.dir/main.cpp.o"
+  "untitled34"
+  "untitled34.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/untitled34.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
